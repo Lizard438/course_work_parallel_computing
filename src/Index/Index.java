ace9aff53a237d039cf2f3cd190f1f2039fa33b6
Lizard438 <lizardmon920@gmail.com>
2021-05-30 13:50:42 +0300

    fix!: changed the form of token position in document

--- src/Index/Index.java
+++ src/Index/Index.java
@@ -1,17 +1,33 @@
 package Index;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class Index {
 
-    ConcurrentHashMap<String, HashSet<Position>>  dictionary;
-    ConcurrentHashMap<Integer,String> docTable;
+    ConcurrentHashMap<String, ArrayList<Position>>  dictionary;
+    HashMap <Integer,String> docTable;
+
+    public Index(){
+        dictionary = new ConcurrentHashMap<>();
+        docTable = new HashMap<>();
+    }
+
+    void addToken(String token, int docId, int lineStart){
+        //
+    }
 
     public static class Position implements Serializable{
+
         int docID;
-        int offset;
-        int lineSize;
+        int lineStart;
+
+        public Position(int docID, int lineStart){
+            this.docID = docID;
+            this.lineStart = lineStart;
+        }
     }
 }
