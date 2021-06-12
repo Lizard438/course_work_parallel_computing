102459ccbbf8c6b755f91a2d3cabf78b3561c0be
Lizard438 <lizardmon920@gmail.com>
2021-06-12 22:45:32 +0300

    feat: added time estimation for different number of threads.

--- src/Index/IndexGenerator.java
+++ src/Index/IndexGenerator.java
@@ -7,12 +7,14 @@ import java.util.ArrayList;
 import java.util.StringTokenizer;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
 
 
 public class IndexGenerator {
 
     ArrayList<CompletableFuture<Void>> tasks = new ArrayList<>();
     Index index = new Index();
+    public static ForkJoinPool pool;
 
 
 
@@ -70,30 +72,74 @@ public class IndexGenerator {
             numerator +=1;
             index.docTable.put(id,file.toString());
 
-            tasks.add(CompletableFuture.runAsync(()->processFile(file, id)));
+            tasks.add(CompletableFuture.runAsync(()->processFile(file, id), pool));
 
             return FileVisitResult.CONTINUE;
 
         }
     }
 
-    public static void main(String []args){
-        IndexGenerator i = new IndexGenerator();
-        try(BufferedReader console = new BufferedReader( new InputStreamReader(System.in)))
-        {
-            System.out.println("Enter path to directory to generate index: ");
-            String path = console.readLine();
+    public static void timeAnalysis(int minThreads, int maxThreads, int step){
 
-            if(path.equals("default")){
-                path = "c:/Users/liza/IdeaProjects/filesCourse";
+        ArrayList<String> result = new ArrayList<>();
+
+        for (int i = minThreads; i < maxThreads; i+=step){
+
+            IndexGenerator gen = new IndexGenerator();
+            pool = new ForkJoinPool(i);
+
+            long startTime = System.currentTimeMillis();
+
+            try{
+                gen.generateIndex("c:/Users/liza/IdeaProjects/filesCourse");
+            }catch (IOException e){
+                e.printStackTrace();
             }
 
-            i.generateIndex(path);
-            System.out.println("Index ready.");
-
-        }catch(IOException e){
-            e.printStackTrace();
+            long endTime = System.currentTimeMillis();
+            pool.shutdown();
+            String line = i+","+(endTime - startTime);
+            result.add(line);
         }
+
+        try (PrintWriter pw = new PrintWriter("c:/Users/liza/IdeaProjects/CourseWork/time.csv")) {
+            result.forEach(pw::println);
+        }catch(FileNotFoundException e){
+            System.out.println("file not found");
+        }
+
+    }
+
+    public static void main(String []args){
+
+        if(args.length != 0){
+            int minThreads = Integer.parseInt(args[0]);
+            int maxThreads = Integer.parseInt(args[1]);
+            int step = Integer.parseInt(args[2]);
+            System.out.println("Time Analysis mode");
+            timeAnalysis(minThreads, maxThreads, step);
+
+        }else{
+
+            IndexGenerator i = new IndexGenerator();
+            pool = ForkJoinPool.commonPool();
+            try(BufferedReader console = new BufferedReader( new InputStreamReader(System.in)))
+            {
+                System.out.println("Enter path to directory to generate index: ");
+                String path = console.readLine();
+
+                if(path.equals("default")){
+                    path = "c:/Users/liza/IdeaProjects/filesCourse";
+                }
+
+                i.generateIndex(path);
+                System.out.println("Index ready.");
+
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+        }
+
     }
 
 }
